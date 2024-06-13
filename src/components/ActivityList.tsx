4f839c2d01ccb25cd89c699cc38c8e4ae3f6271b
Nahuel Stella <nahuelprg@gmail.com>
2024-06-13 17:44:11 -0300

    Contador de Calorias terminado

---
+++ src/components/ActivityList.tsx
@@ -0,0 +1,62 @@
+import { Dispatch, useMemo } from "react";
+import { ActivityActions } from "../reducers/activity-reducer";
+import { categories } from "../data/categories";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/solid";
+import { Activity } from "../types";
+
+type ActivityListProps = {
+    activities: Activity[],
+    dispatch: Dispatch<ActivityActions>
+}
+
+export default function ActivityList({activities, dispatch}: ActivityListProps) {
+    
+    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities]);
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities]);
+    return (
+        <>
+            <h2 className=" text-4xl font-bold text-white text-center">
+                Comida y Actividades
+            </h2>
+
+            {isEmptyActivities ? 
+                <p className="text-center my-5 text-sm text-gray-400">No Hay Actividades aún...</p> :
+            activities.map(activity => (
+                <div key={activity.id} className="px-5 py-10  bg-white mt-5 flex justify-between shadow">
+                    
+                    <div className="space-y-2 relative">
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                            ${activity.category === 1 ? 'bg-green-500' : 'bg-yellow-500'
+                        }`}>
+                            {categoryName(+activity.category)}
+                        </p>
+                        <p className="text-2xl font-bold pt-5">{activity.name}</p>
+                        <p className="font-black text-4xl text-neutral-600 ">
+                            {activity.calories} {' '}
+                            <span>Calorias</span>
+                        </p>
+                    </div>
+
+                    <div className="flex gap-5 items-center">
+                        <button
+                            onClick={() => dispatch({type: 'set-activeId', payload: {id: activity.id}})}
+                        >
+                            <PencilSquareIcon
+                                className="h-8 w-8 text-indigo-800"
+                            />
+                        </button>
+                        
+                        <button
+                            onClick={() => dispatch({type: 'delete-activity', payload: {id: activity.id}})}
+                        >
+                            <XCircleIcon
+                                className="h-8 w-8 text-red-600"
+                            />
+                        </button>
+                    </div>
+
+                </div>
+            ))}
+        </>
+    )
+}
